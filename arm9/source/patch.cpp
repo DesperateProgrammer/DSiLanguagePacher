e1bd64eb3d0ca8af1c7da06818015dde016543fa
Tim Seidel <t.seidel@kts-systeme.de>
2021-03-17 09:57:12 +0100

    - added method to apply selected options to patches

--- arm9/source/patch.cpp
+++ arm9/source/patch.cpp
@@ -25,6 +25,19 @@ void patch_applyPatternPatches(uint8_t *target, uint32_t size,
 	}
 }
  
+static uint32_t patch_findOption(std::vector<SOPTIONSELECT> &options, const char *name)
+{
+  for (uint32_t i=0;i<options.size();i++)
+  {
+    if (strcmp(name, options[i].name) == 0)
+    {
+      return i ;
+    }
+  }
+  return (uint32_t)-1;
+}
+
+
 static uint32_t patch_addOrFindOption(std::vector<SOPTIONSELECT> &options, const char *name)
 {
   for (uint32_t i=0;i<options.size();i++)
@@ -38,7 +51,7 @@ static uint32_t patch_addOrFindOption(std::vector<SOPTIONSELECT> &options, const
   entry.name = name ;
   entry.selection = 0 ;
   options.push_back(entry) ;
-  return patch_addOrFindOption(options, name) ;
+  return patch_findOption(options, name) ;
 }
 
 static uint32_t patch_addorFindOptionValue(std::vector<const char *> &values, const char *value)
@@ -71,4 +84,29 @@ std::vector<SOPTIONSELECT> patch_getAvailableOptions(const SPATCHLISTENTRY *patc
     }
   }  
   return options;
+}
+
+void patch_applySelectedOptions(uint8_t *target, uint32_t size, 
+                                const SPATCHLISTENTRY *patches, const SPATCHRESULT *patchResult,
+                                const uint32_t count, std::vector<SOPTIONSELECT> selectedOptions)
+{
+  std::vector<SOPTIONSELECT> options ;
+  for (uint32_t patch = 0; patch < count; patch++)
+  {
+    for (uint32_t optionNum = 0; optionNum < patches[patch].optionCount; optionNum++)
+    {
+      uint32_t optionIndex = patch_findOption(selectedOptions, patches[patch].options[optionNum].name) ;
+      if (optionIndex == (uint32_t)-1)
+        continue ;
+      if (strcmp(patches[patch].options[optionNum].value, selectedOptions[optionIndex].values[selectedOptions[optionIndex].selection]) == 0)
+      {
+        // this is the selected option
+        if (patchResult[patch].matchCount)
+        {
+          memcpy(target + patchResult[patch].lastFoundOffset + patches[patch].options[optionNum].inPatchOffset, 
+                  patches[patch].options[optionNum].inPatchChange, patches[patch].options[optionNum].inPatchSize) ;
+        }
+      }
+    }
+  }  
 }
